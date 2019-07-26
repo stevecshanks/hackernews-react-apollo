69b9b1e57863d717529138a804145f7d04ac6e0a
Steve Shanks <steve.c.shanks@gmail.com>
2019-07-26 15:22:16 +0100

    Complete more mutations

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,7 +3,7 @@ import Link from './Link'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -11,6 +11,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -27,13 +37,29 @@ class LinkList extends Component {
             const linksToRender = data.feed.links
       
             return (
-              <div>
-                {linksToRender.map(link => <Link key={link.id} link={link} />)}
-              </div>
-            )
+                <div>
+                  {linksToRender.map((link, index) => (
+                    <Link
+                        key={link.id}
+                        link={link}
+                        index={index}
+                        updateStoreAfterVote={this._updateCacheAfterVote}
+                    />
+                  ))}
+                </div>
+              )
           }}
         </Query>
       )
+    }
+
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+      // TODO: this does not work, seems to be a known issue in Apollo In Memory Cache: https://github.com/apollographql/apollo-client/issues/4031
+      const data = store.readQuery({query: FEED_QUERY})
+  
+      const votedLink = data.feed.links.find(link => link.id === linkId)
+      votedLink.votes = createVote.link.votes
+      store.writeQuery({ query: FEED_QUERY, data })
     }
   }
 
