336176c07c9369c3f1fac99abf0cb192cae88323
Steve Shanks <steve.c.shanks@gmail.com>
2019-07-26 18:17:01 +0100

    Complete subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -26,41 +26,119 @@ export const FEED_QUERY = gql`
   }
 `
 
-class LinkList extends Component {
-    render() {
-      return (
-        <Query query={FEED_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return <div>Fetching</div>
-            if (error) return <div>Error</div>
-      
-            const linksToRender = data.feed.links
-      
-            return (
-                <div>
-                  {linksToRender.map((link, index) => (
-                    <Link
-                        key={link.id}
-                        link={link}
-                        index={index}
-                        updateStoreAfterVote={this._updateCacheAfterVote}
-                    />
-                  ))}
-                </div>
-              )
-          }}
-        </Query>
-      )
-    }
-
-    _updateCacheAfterVote = (store, createVote, linkId) => {
-      // TODO: this does not work, seems to be a known issue in Apollo In Memory Cache: https://github.com/apollographql/apollo-client/issues/4031
-      const data = store.readQuery({query: FEED_QUERY})
-  
-      const votedLink = data.feed.links.find(link => link.id === linkId)
-      votedLink.votes = createVote.link.votes
-      store.writeQuery({ query: FEED_QUERY, data })
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
     }
   }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
+class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    // TODO: this does not work, seems to be a known issue in Apollo In Memory Cache: https://github.com/apollographql/apollo-client/issues/4031
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+  
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
+  render() {
+    return (
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data, subscribeToMore }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+
+          this._subscribeToNewLinks(subscribeToMore)
+          this._subscribeToNewVotes(subscribeToMore)
+
+          const linksToRender = data.feed.links
+
+          return (
+            <div>
+              {linksToRender.map((link, index) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
+              ))}
+            </div>
+          )
+        }}
+      </Query>
+    )
+  }
+}
 
 export default LinkList
--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ import { AUTH_TOKEN } from './constants'
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -27,8 +31,27 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
