ae4704f273ad7567fe9a18b4a1accc0a3acd8654
Steve Shanks <steve.c.shanks@gmail.com>
2019-07-26 10:52:17 +0100

    Complete authentication

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 import { BrowserRouter } from 'react-router-dom'
 
@@ -15,8 +17,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
