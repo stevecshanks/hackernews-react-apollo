2e2a13064fdfde3c87123d1afe1d877e6412318b
Steve Shanks <steve.c.shanks@gmail.com>
2019-07-26 09:49:37 +0100

    Complete routing

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+import { BrowserRouter } from 'react-router-dom'
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -19,9 +21,11 @@ const client = new ApolloClient({
 })
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
 
