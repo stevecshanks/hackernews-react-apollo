1117e1f0986c287eb07fb2dc20fed1e2e5faa784
Steve Shanks <steve.c.shanks@gmail.com>
2019-08-07 11:59:42 +0100

    Update search to use hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
 import Link from './Link'
@@ -26,40 +26,33 @@ const FEED_SEARCH_QUERY = gql`
   }
 `
 
-class Search extends Component {
+function Search({ client }) {
+  const [filter, setFilter] = useState('')
+  const [links, setLinks] = useState([])
 
-  state = {
-    links: [],
-    filter: ''
-  }
-
-  render() {
-    return (
-      <div>
-        <div>
-          Search
-          <input
-            type='text'
-            onChange={e => this.setState({ filter: e.target.value })}
-          />
-          <button onClick={() => this._executeSearch()}>OK</button>
-        </div>
-        {this.state.links.map((link, index) => (
-          <Link key={link.id} link={link} index={index} />
-        ))}
-      </div>
-    )
-  }
-
-  _executeSearch = async () => {
-    const { filter } = this.state
-    const result = await this.props.client.query({
+  const executeSearch = async () => {
+    const result = await client.query({
       query: FEED_SEARCH_QUERY,
       variables: { filter },
     })
-    const links = result.data.feed.links
-    this.setState({ links })
+    setLinks(result.data.feed.links)
   }
+
+  return (
+    <div>
+      <div>
+        Search
+        <input
+          type='text'
+          onChange={e => setFilter(e.target.value)}
+        />
+        <button onClick={() => executeSearch()}>OK</button>
+      </div>
+      {links.map((link, index) => (
+        <Link key={link.id} link={link} index={index} />
+      ))}
+    </div>
+  )
 }
 
 export default withApollo(Search)
