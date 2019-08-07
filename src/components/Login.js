8f95bd3afef7aba2cc8c8a506ba1947e9a9cdff5
Steve Shanks <steve.c.shanks@gmail.com>
2019-08-07 12:15:45 +0100

    Update Login to use hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { AUTH_TOKEN } from '../constants'
 import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo';
@@ -19,73 +19,69 @@ const LOGIN_MUTATION = gql`
   }
 `
 
-class Login extends Component {
-  state = {
-    login: true, // switch between Login and SignUp
-    email: '',
-    password: '',
-    name: '',
-  }
+function Login({ history }) {
 
-  render() {
-    const { login, email, password, name } = this.state
-    return (
-      <div>
-        <h4 className="mv3">{login ? 'Login' : 'Sign Up'}</h4>
-        <div className="flex flex-column">
-          {!login && (
-            <input
-              value={name}
-              onChange={e => this.setState({ name: e.target.value })}
-              type="text"
-              placeholder="Your name"
-            />
-          )}
-          <input
-            value={email}
-            onChange={e => this.setState({ email: e.target.value })}
-            type="text"
-            placeholder="Your email address"
-          />
-          <input
-            value={password}
-            onChange={e => this.setState({ password: e.target.value })}
-            type="password"
-            placeholder="Choose a safe password"
-          />
-        </div>
-        <div className="flex mt3">
-            <Mutation
-                mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
-                variables={{ email, password, name }}
-                onCompleted={data => this._confirm(data)}
-            >
-                {mutation => (
-                <div className="pointer mr2 button" onClick={mutation}>
-                    {login ? 'login' : 'create account'}
-                </div>
-                )}
-            </Mutation>
-            <div
-                className="pointer button"
-                onClick={() => this.setState({ login: !login })}
-            >
-                {login ? 'need to create an account?' : 'already have an account?'}
-            </div>
-        </div>
-      </div>
-    )
-  }
+  const [isLogin, setIsLogin] = useState(true)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
 
-  _confirm = async data => {
-    const { token } = this.state.login ? data.login : data.signup
-    this._saveUserData(token)
-    this.props.history.push(`/`)
-  }
-
-  _saveUserData = token => {
+  const saveUserData = token => {
     localStorage.setItem(AUTH_TOKEN, token)
   }
+
+  const confirm = async data => {
+    const { token } = isLogin ? data.login : data.signup
+    saveUserData(token)
+    history.push(`/`)
+  }
+
+  return (
+    <div>
+      <h4 className="mv3">{isLogin ? 'Login' : 'Sign Up'}</h4>
+      <div className="flex flex-column">
+        {!isLogin && (
+          <input
+            value={name}
+            onChange={e => setName(e.target.value)}
+            type="text"
+            placeholder="Your name"
+          />
+        )}
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type="text"
+          placeholder="Your email address"
+        />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          placeholder="Choose a safe password"
+        />
+      </div>
+      <div className="flex mt3">
+          <Mutation
+              mutation={isLogin ? LOGIN_MUTATION : SIGNUP_MUTATION}
+              variables={{ email, password, name }}
+              onCompleted={data => confirm(data)}
+          >
+              {mutation => (
+              <div className="pointer mr2 button" onClick={mutation}>
+                  {isLogin ? 'login' : 'create account'}
+              </div>
+              )}
+          </Mutation>
+          <div
+              className="pointer button"
+              onClick={() => setIsLogin(!isLogin)}
+          >
+              {isLogin ? 'need to create an account?' : 'already have an account?'}
+          </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
